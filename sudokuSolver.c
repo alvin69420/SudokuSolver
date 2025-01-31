b5fc2e598fb8044f1d3fe5fb9258865c9ee004e4
Alvin Tran <101605216+alvin69420@users.noreply.github.com>
2025-01-30 21:05:47 -0500

    finished sudokusolver

--- sudokuSolver.c
+++ sudokuSolver.c
@@ -3,12 +3,13 @@
 #define N 9
 
 void findAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availableNumbers[N]);
-
-void resetAvailableNumbers (int availableNumbers[N])//TODO
+void resetAvailableNumbers (int availableNumbers[N]);
+int tryAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availableNumbers[N]);
+void displayBoardState(int sudokuBoard[N][N]);
 
 int main() {
     
-    int availableNumbers[N] = {1};
+    int availableNumbers[N];
     int sudokuBoard[N][N] = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
         {6, 0, 0, 1, 9, 5, 0, 0, 0},
@@ -20,31 +21,15 @@ int main() {
         {0, 0, 0, 4, 1, 9, 0, 0, 5},
         {0, 0, 0, 0, 8, 0, 0, 7, 9}
     };
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (sudokuBoard[i][j] != 0) {
-                findAvailableNumbers(i, j, sudokuBoard, availableNumbers);
-            }
-        }
-    }
-    
-    for (int i = 0; i < N; i++) {
-        availableNumbers[i] = 1;
-    }
-    
-    findAvailableNumbers(7,7, sudokuBoard, availableNumbers);
-    
-    for (int i = 0; i < N; i++) {
-        printf("%d", availableNumbers[i]);
-    }
+
+    tryAvailableNumbers(0, 0, sudokuBoard, availableNumbers);
+
+    displayBoardState(sudokuBoard);
     
     return 0;
 }
 
-int* findAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availableNumbers[N]) {
-    
-    int currentSquare = 0;
+void findAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availableNumbers[N]) {
     
     for(int i = 0; i < N; i++) {
         if(sudokuBoard[row][i] != 0) {
@@ -55,11 +40,10 @@ int* findAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availa
         }
     }
     
-    currentSquare = (row / 3) * 3 + (column / 3);
-    
+    int currentSquare = (row / 3) * 3 + (column / 3);
     int rowOffset = (currentSquare / 3) * 3;
     int columnOffset = (currentSquare % 3) * 3;
-    
+
     for (int row = 0; row < 3; row++) {
         for(int column = 0; column < 3; column++) {
             if (sudokuBoard[row + rowOffset][column + columnOffset] != 0) {
@@ -68,3 +52,63 @@ int* findAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availa
         }
     }
 }    
+
+void resetAvailableNumbers (int availableNumbers[N]) {
+    for (int i = 0; i < N; i++) {
+        availableNumbers[i] = 1;
+    }
+}
+
+int tryAvailableNumbers(int row, int column, int sudokuBoard[N][N], int availableNumbers[N]) {
+
+    int nextRow;
+    int nextColumn;
+
+    if (row == N) return 1;
+
+    if (column == 8) {
+        nextRow = row + 1;
+        nextColumn = 0;
+    } else {
+        nextRow = row;
+        nextColumn = column + 1;
+    }
+
+    if (sudokuBoard[row][column] != 0) {
+        return tryAvailableNumbers(nextRow, nextColumn, sudokuBoard, availableNumbers);
+    }
+
+    for (int i = 0; i < N; i++) {
+        resetAvailableNumbers(availableNumbers);
+        findAvailableNumbers(row, column, sudokuBoard, availableNumbers);
+        if (availableNumbers[i] != 0) {
+            sudokuBoard[row][column] = i+1;
+            if (tryAvailableNumbers(nextRow, nextColumn, sudokuBoard, availableNumbers)) {
+                return 1;
+            }
+        }
+    }
+    sudokuBoard[row][column] = 0;
+    
+    for(int i = 0; i < N; i++) {
+        printf("%d", availableNumbers[i]);
+    }
+
+    displayBoardState(sudokuBoard);
+    return 0;
+}
+
+void displayBoardState(int board[N][N]) {
+    int lastColumnIndex = N - 1;
+
+    printf("\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < lastColumnIndex; j++) {
+            if (((j + 1) % 3) != 0) printf("%c ", (board[i][j] == 0 ? ' ' : board[i][j] + '0'));
+            else printf("%c || ", (board[i][j] == 0 ? ' ' : board[i][j] + '0'));
+        }
+        if (((i + 1) % 3) != 0 || i == N - 1) printf("%c\n", board[i][lastColumnIndex] == 0 ? ' ' : board[i][lastColumnIndex] + '0');
+        else printf("%c\n=======================\n", board[i][lastColumnIndex] == 0 ? ' ' : board[i][lastColumnIndex] + '0');
+    }
+    printf("\n");
+}
